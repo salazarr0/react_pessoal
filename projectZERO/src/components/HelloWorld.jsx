4b084e5309e5b48e18afac345ecbc398c27ac300
salazarr0 <samuelguimaraes091@gmail.com>
2026-02-26 16:14:27 -0300

    Simple codes in app.jsx and a vite model for reference.

---
+++ projectZERO/src/components/HelloWorld.jsx
@@ -0,0 +1,14 @@
+import World from "./World"
+function HelloWorld(){
+
+        return(
+            <>
+                <div>
+                    <h1>Hello World!</h1>
+                    <World/>
+                </div>
+            </>
+        )
+}
+
+export default HelloWorld
---
+++ projectZERO/src/components/World.jsx
@@ -0,0 +1,12 @@
+function World(){
+
+        return(
+            <>
+                <div>
+                    <p>Its a component with a world!</p>
+                </div>
+            </>
+        )
+}
+
+export default World
